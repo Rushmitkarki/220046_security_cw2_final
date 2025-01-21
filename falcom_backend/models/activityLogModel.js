d514f2407d100845dc5b2eddbc647f7b0a1f7b72
Rushmitkarki <220046@softwarica.edu.np>
2025-01-21 22:50:40 +0545

    changes activity model

--- falcom_backend/models/activityLogModel.js
+++ falcom_backend/models/activityLogModel.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const activityLogSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
-  action: { type: String, required: true }, // e.g., "login", "login_failed", "password_reset", "profile_update"
+  action: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
   ipAddress: { type: String },
-  details: { type: mongoose.Schema.Types.Mixed }, // Additional details like OTP attempts, etc.
+  details: { type: mongoose.Schema.Types.Mixed },
 });
 
 const ActivityLog = mongoose.model("ActivityLog", activityLogSchema);
